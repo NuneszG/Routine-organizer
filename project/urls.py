0931b7aa8d261807c886f9b34d11c50d479e46cf
GuilhermeNunes <digobros13@gmail.com>
2024-11-19 15:17:13 -0300

    created timer into specific task

--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from .views import (
     Home,
     TasksPage,
+    Form,
+    Create,
+    GetTask,
 )
 
 urlpatterns = [
     path('', Home, name='home'),
     path('Tasks/', TasksPage, name='tasks'),
+    path('Create/', Create, name='create'),
+    path('Form/', Form, name='form'),
+    path('GetTask/<int:id>/', GetTask, name='task'),
 ]
--- project/views.py
+++ project/views.py
@@ -9,3 +9,24 @@ def TasksPage(request):
     task = Task.objects.all()
     
     return render(request, 'tasks.html', {'task': task})
+
+def Form(request):
+    return render(request, 'form.html')
+
+def Create(request):
+    title = request.POST.get('title')
+    caption = request.POST.get('caption')
+    content = request.POST.get('content')
+
+    task = Task.objects.create(
+        title = title,
+        caption = caption,
+        content = content
+    )
+
+    return render(request, 'tasks.html', {'task': task})
+
+def GetTask(request, id):
+    task = Task.objects.get(id=id)
+
+    return render(request, 'task.html', {'task': task})
